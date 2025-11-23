e4cb679e866e0f03030442f2c5ede084847b9201
Eren Siegman <eren.siegman@esiegman.dev>
2025-11-23 13:44:09 -0500

    AUTO-COMMIT: Pre-switch state before Nix rebuild on

--- modules/nixos/dev.nix
+++ modules/nixos/dev.nix
@@ -64,4 +64,17 @@ let
 in
 {
   environment.systemPackages = languageToolchains ++ formattersAndLinters ++ generalDevUtils;
+
+  programs.nix-ld.enable = true;
+
+programs.nix-ld.libraries = with pkgs; [
+  stdenv.cc.cc
+  zlib
+  fuse3
+  icu
+  nss
+  openssl
+  curl
+  expat
+];
 }
