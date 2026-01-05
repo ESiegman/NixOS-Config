264bc844d785d472dd4aca2a2531a52251034e0d
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-05 18:32:25 -0500

    AUTO-COMMIT: Pre-switch state before Nix rebuild on

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -112,7 +112,7 @@ in {
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
   boot.kernelModules = [ "v4l2loopback" ];
   boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="Virtual Camera" exclusive_caps=1
+    options v4l2loopback devices=2 video_nr=1,10 card_label="OBS-Raw","Discord-Ready" exclusive_caps=1
   '';
   programs.obs-studio.enableVirtualCamera = true;
 }
