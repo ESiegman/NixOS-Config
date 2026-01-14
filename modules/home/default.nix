4ee82144f9c390e6e5077343b36f1a345a9a4446
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-14 13:02:31 -0500

    rebuild: 2026-01-14 13:02:31

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,15 +1,21 @@
 { osConfig, ... }: {
-  imports =
-    [ ./zsh.nix ./starship.nix ./fastfetch.nix ./firefox.nix ./vesktop.nix ./neovim.nix ]
-    ++ (if osConfig.networking.hostName == "desktop" then [
-      ./hypr/hyprland.nix
-      ./hypr/hyprlock.nix
-      ./hypr/swaync.nix
-      ./hypr/wofi.nix
-    ] else if osConfig.networking.hostName == "laptop" then
-      [ ./cinnamon.nix ]
-    else
-      [ ]);
+  imports = [
+    ./zsh.nix
+    ./starship.nix
+    ./fastfetch.nix
+    ./firefox.nix
+    ./vesktop.nix
+    ./neovim.nix
+    ./kitty.nix
+  ] ++ (if osConfig.networking.hostName == "desktop" then [
+    ./hypr/hyprland.nix
+    ./hypr/hyprlock.nix
+    ./hypr/swaync.nix
+    ./hypr/wofi.nix
+  ] else if osConfig.networking.hostName == "laptop" then
+    [ ./cinnamon.nix ]
+  else
+    [ ]);
 
   stylix.targets = {
     firefox.enable = false;
--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -12,7 +12,6 @@
 
       window_padding_width = 15;
       confirm_os_window_close = 0;
-      "shell_integration" = "no-rc";
 
       detect_urls = true;
       enable_audio_bell = false;
