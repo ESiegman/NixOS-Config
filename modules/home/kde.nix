c4b869381c824b4528b1f312b01b83e4cd05e7e0
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-15 14:02:20 -0500

    rebuild: 2026-01-15 14:02:20

--- modules/home/kde.nix
+++ modules/home/kde.nix
@@ -1,8 +1,13 @@
 # modules/home/kde.nix
 { pkgs, ... }: {
-  programs.plasma = {
-    enable = true;
-    configFile."kdeglobals"."General"."TerminalApplication" = "kitty";
-    configFile."kdeglobals"."General"."TerminalService" = "kitty.desktop";
+  programs.plasma = { enable = true; 
+    panels = {
+      {
+        location = "bottom";
+        widgets = [
+
+        ];
+      }
+    };
   };
 }
