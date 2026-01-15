4b3a24be065ce299f1b9da90fb4babd9cfb97475
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-15 14:36:45 -0500

    rebuild: 2026-01-15 14:36:45

--- modules/home/kde.nix
+++ modules/home/kde.nix
@@ -2,44 +2,82 @@
 { pkgs, ... }: {
   programs.plasma = {
     enable = true;
-    panels = [{
-      location = "bottom";
-      widgets = [
-        {
-          kickoff = {
-            icon = "nix-snowflake-white";
-            sortAlphabetically = true;
+    panels = [
+      {
+        location = "bottom";
+        widgets = [
+          {
+            kickoff = {
+              icon = "nix-snowflake-white";
+              sortAlphabetically = true;
+            };
+          }
+          {
+            iconTasks = {
+              launchers = [
+                "applications:org.kde.dolphin.desktop"
+                "applications:firefox.desktop"
+                "applications:kitty.desktop"
+                "applications:vesktop.desktop"
+                "applications:spotify.desktop"
+              ];
+            };
+          }
+          "org.kde.plasma.marginsseparator"
+          {
+            systemTray.items = {
+              shown = [
+                "org.kde.plasma.bluetooth"
+                "org.kde.plasma.networkmanagement"
+                "org.kde.plasma.battery"
+                "org.kde.plasma.volume"
+              ];
+              hidden = [ "org.kde.plasma.clipboard" ];
+            };
+          }
+          {
+            digitalClock = {
+              calendar.firstDayOfWeek = "sunday";
+              time.format = "12h";
+            };
+          }
+        ];
+      }
+      {
+        location = "top";
+        widgets = [{
+          applicationTitleBar = {
+            behavior = { activeTaskSource = "activeTask"; };
+            layout = {
+              elements = [ "windowTitle" ];
+              horizontalAlignment = "left";
+              showDisabledElements = "deactivated";
+              verticalAlignment = "center";
+            };
+            overrideForMaximized.enable = false;
+            titleReplacements = [{
+              type = "regexp";
+              originalTitle = "\\\\bDolphin\\\\b";
+              newTitle = "File manager";
+            }];
+            windowTitle = {
+              font = {
+                bold = false;
+                fit = "fixedSize";
+                size = 12;
+              };
+              hideEmptyTitle = true;
+              margins = {
+                bottom = 0;
+                left = 10;
+                right = 5;
+                top = 0;
+              };
+              source = "appName";
+            };
           };
-        }
-        {
-          iconTasks = {
-            launchers = [
-              "applications:org.kde.preferences.desktop"
-              "applications:org.kde.dolphin.desktop"
-              "applications:kitty.desktop"
-              "applications:firefox.desktop"
-            ];
-          };
-        }
-        "org.kde.plasma.marginsseparator"
-        {
-          systemTray.items = {
-            shown = [
-              "org.kde.plasma.bluetooth"
-              "org.kde.plasma.networkmanagement"
-              "org.kde.plasma.battery"
-              "org.kde.plasma.volume"
-            ];
-            hidden = [ "org.kde.plasma.clipboard" ];
-          };
-        }
-        {
-          digitalClock = {
-            calendar.firstDayOfWeek = "sunday";
-            time.format = "12h";
-          };
-        }
-      ];
-    }];
+        }];
+      }
+    ];
   };
 }
