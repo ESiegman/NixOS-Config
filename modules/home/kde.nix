7e37ceaa79a88bc806712477f3edd838f43c461e
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-15 14:05:34 -0500

    rebuild: 2026-01-15 14:05:34

--- modules/home/kde.nix
+++ modules/home/kde.nix
@@ -1,13 +1,16 @@
 # modules/home/kde.nix
 { pkgs, ... }: {
-  programs.plasma = { enable = true; 
-    panels = {
-      {
-        location = "bottom";
-        widgets = [
-
-        ];
-      }
-    };
+  programs.plasma = {
+    enable = true;
+    panels = [{
+      location = "bottom";
+      widgets = [{
+        name = "org.kde.plasma.kickoff";
+        config = {
+          icon = "nix-snowflake-white";
+          alphaSort = true;
+        };
+      }];
+    }];
   };
 }
