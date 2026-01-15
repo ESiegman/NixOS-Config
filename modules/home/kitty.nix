e6bad29b70d3ea11f8378652a8f62c0af1139fa2
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-15 12:35:14 -0500

    rebuild: 2026-01-15 12:35:14

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -8,6 +8,10 @@
     in if isLaptop then
       pkgs.writeShellScriptBin "kitty" ''
         #!/bin/sh
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
         exec nvidia-offload ${baseKitty}/bin/kitty "$@"
       ''
     else
