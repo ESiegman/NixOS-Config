83f1bec145889bc2a82ada060170c6217c907a48
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-15 11:00:46 -0500

    rebuild: 2026-01-15 11:00:46

--- modules/home/kde.nix
+++ modules/home/kde.nix
@@ -1,4 +1,14 @@
 # modules/home/kde.nix
-{ ... }: {
-  programs.plasma = { enable = true; };
+{ pkgs, ... }: {
+  programs.plasma = {
+    enable = true;
+    configFile."kdeglobals"."General"."TerminalApplication" = "kitty";
+    configFile."kdeglobals"."General"."TerminalService" = "kitty.desktop";
+  };
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole
+    elisa
+    kate
+    okular
+  ];
 }
