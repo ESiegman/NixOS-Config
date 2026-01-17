c6ee5f395f78143e10ee1a52dd63ce532f803bdf
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-17 15:22:00 -0500

    rebuild: 2026-01-17 15:22:00

--- modules/home/vesktop.nix
+++ modules/home/vesktop.nix
@@ -1,4 +1,34 @@
 # home/eren/vesktop.nix
-{ ... }: {
-  programs.vesktop = { enable = true; };
+{...}: {
+  programs.vesktop = {
+    enable = true;
+    vencord = {
+      settings = {
+        plugins = {
+          MessageLogger.enabled = true;
+          AlwaysAnimate.enabled = true;
+          ClearURLs.enabled = true;
+          FixCodeblockGap = true;
+          ForceOwnerCrown = true;
+          FriendsSince = true;
+          FullSearchContext = true;
+          ImageFilename = true;
+          ImplicitRelationships = true;
+          MemberCount = true;
+          MessageClickActions = true;
+          oneko = true;
+          petpet = true;
+          ReadAllNotificationsButton = true;
+          ServerInfo = true;
+          ShikiCodeblocks = true;
+          ShowHiddenChannels = true;
+          SilentTyping = true;
+        };
+        enabledThemes = ["Main"];
+      };
+      themes = {
+        Main = builtins.readFile ./../native/vesktop/theme.css;
+      };
+    };
+  };
 }
