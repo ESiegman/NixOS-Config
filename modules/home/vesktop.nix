819974e216f4332017c8e45f7f0c0cc38963d1ab
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-17 15:36:05 -0500

    rebuild: 2026-01-17 15:36:05

--- modules/home/vesktop.nix
+++ modules/home/vesktop.nix
@@ -24,7 +24,7 @@
           ShowHiddenChannels = true;
           SilentTyping = true;
         };
-        enabledThemes = ["Main"];
+        enabledThemes = ["Main.css"];
       };
       themes = {
         "Main.css" = builtins.readFile ./../native/vesktop/theme.css;
