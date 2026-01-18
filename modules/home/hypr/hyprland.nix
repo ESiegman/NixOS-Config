69137e83d28089994aabdac31df0ad57b3ff3734
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-18 18:34:52 -0500

    rebuild: 2026-01-18 18:34:52

--- modules/home/hypr/hyprland.nix
+++ modules/home/hypr/hyprland.nix
@@ -59,12 +59,21 @@ in {
 
       windowrulev2 = [
         "opacity 0.90 0.50, class:^(kitty)$"
-        "float, class:.*(pavucontrol).*"
-        "float, class:.*(blueman-manager).*"
-        "float, class:.*(nmtui-floating).*"
-        "center, class:.*(nmtui-floating).*"
-        "size 600 400, class:.*(nmtui-floating).*"
-        "float, class:.*(thunar).*"
+
+        "float, class:(pavucontrol)"
+        "float, initialClass:(pavucontrol)"
+        "float, initialTitle:(Volume Control)"
+
+        "float, class:(blueman-manager)"
+        "float, initialClass:(blueman-manager)"
+        "float, initialTitle:(Bluetooth Devices)"
+
+        "float, class:(nmtui-floating)"
+        "float, initialClass:(nmtui-floating)"
+        "float, initialTitle:(nmtui-floating)"
+
+        "float, class:(thunar)"
+        "float, initialClass:(thunar)"
       ];
 
       animations = {
