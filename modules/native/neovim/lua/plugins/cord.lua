1190258c27e57e6618343fbc0dbabdb991c8e630
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-19 17:16:49 -0500

    rebuild: 2026-01-19 17:16:49

--- modules/native/neovim/lua/plugins/cord.lua
+++ modules/native/neovim/lua/plugins/cord.lua
@@ -4,12 +4,7 @@ return {
 			"vyfor/cord.nvim",
 			build = ":Cord update",
 			lazy = false,
-			opts = {
-				display = {
-					theme = "catpuccin",
-					flavor = "dark",
-				},
-			},
+			opts = {},
 		},
 	},
 }
