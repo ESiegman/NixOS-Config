6f4b145a20c27ed730ff5a600882b736a6fb0339
Eren Siegman <eren.siegman@esiegman.dev>
2026-01-19 17:23:16 -0500

    rebuild: 2026-01-19 17:23:16

--- modules/native/neovim/lua/plugins/lspconfig.lua
+++ modules/native/neovim/lua/plugins/lspconfig.lua
@@ -18,42 +18,52 @@ return {
 						"--query-driver=/nix/store/*",
 					},
 				},
+				nil_ls = {},
+				rust_analyzer = {},
+				pyright = {},
+				ts_ls = {},
+				html = {},
+				cssls = {},
+				jsonls = {},
+				astro = {},
 				lua_ls = {
 					settings = {
 						Lua = {
 							diagnostics = { globals = { "vim" } },
-							workspace = { library = { vim.env.VIMRUNTIME } },
+							workspace = {
+								checkThirdParty = false,
+								library = { vim.env.VIMRUNTIME },
+							},
 						},
 					},
 				},
+				bashls = {},
+				yamlls = {},
+				marksman = {},
 			}
 
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 			vim.diagnostic.config({
 				underline = true,
-				virtual_text = true,
+				virtual_text = { spacing = 4, prefix = "●" },
 				signs = true,
 				update_in_insert = true,
+				severity_sort = true,
 			})
 
 			for server_name, server_config in pairs(servers) do
 				local config_mod = require("lspconfig.configs." .. server_name)
-				local final_config = vim.tbl_deep_extend("force", config_mod.default_config, server_config, {
+				local default_config = config_mod.default_config
+
+				local final_config = vim.tbl_deep_extend("force", default_config, server_config, {
 					capabilities = capabilities,
-					single_file_support = true,
 				})
 
 				vim.lsp.config(server_name, final_config)
+
 				vim.lsp.enable(server_name)
 			end
-
-			vim.api.nvim_create_autocmd("LspAttach", {
-				callback = function(args)
-					local client = vim.lsp.get_client_by_id(args.data.client_id)
-					vim.notify("LSP Attached: " .. client.name)
-				end,
-			})
 		end,
 	},
 }
