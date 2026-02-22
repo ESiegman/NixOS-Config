9983d2d6e0ce2056ca9b9cf431097bc67a241bdc
Eren Siegman <eren.siegman@esiegman.dev>
2026-02-22 12:51:43 -0500

    rebuild: 2026-02-22 12:51:43

--- modules/native/neovim/lua/config/cpp_snippets.lua
+++ modules/native/neovim/lua/config/cpp_snippets.lua
@@ -8,11 +8,11 @@ local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("cpp", {
 
--- =========================================================
--- Competitive Programming
--- =========================================================
+    -- =========================================================
+    -- Competitive Programming
+    -- =========================================================
 
-s("main20", fmt([[
+    s("compmain", fmt([[
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -26,12 +26,12 @@ int main() {{
 }}
 ]], { i(0) })),
 
-s("fastio", fmt([[
+    s("fastio", fmt([[
 ios::sync_with_stdio(false);
 cin.tie(nullptr);
 ]], {})),
 
-s("cpdefs", fmt([[
+    s("cpdefs", fmt([[
 using ll = long long;
 using ull = unsigned long long;
 using pii = pair<int,int>;
@@ -40,7 +40,7 @@ constexpr int INF = 1e9;
 constexpr long long LINF = 1e18;
 ]], {})),
 
-s("pbds", fmt([[
+    s("pbds", fmt([[
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
@@ -54,7 +54,7 @@ using ordered_set = tree<
     tree_order_statistics_node_update>;
 ]], {})),
 
-s("bs", fmt([[
+    s("bs", fmt([[
 int l = 0, r = {} - 1;
 while (l <= r) {{
     int mid = l + (r - l) / 2;
@@ -66,7 +66,7 @@ while (l <= r) {{
 }}
 ]], { i(1, "n"), i(0, "condition") })),
 
-s("dbgcp", fmt([[
+    s("dbgcp", fmt([[
 #ifdef LOCAL
 #define dbg(x) cerr << #x << " = " << (x) << endl;
 #else
@@ -74,7 +74,7 @@ s("dbgcp", fmt([[
 #endif
 ]], {})),
 
-s("bench", fmt([[
+    s("bench", fmt([[
 template<typename F>
 void benchmark(std::string_view name, F&& fn) {{
     auto start = std::chrono::high_resolution_clock::now();
@@ -85,25 +85,25 @@ void benchmark(std::string_view name, F&& fn) {{
 }}
 ]], {})),
 
--- =========================================================
--- Modern Templates / Meta
--- =========================================================
+    -- =========================================================
+    -- Modern Templates / Meta
+    -- =========================================================
 
-s("tfunc", fmt([[
+    s("tfunc", fmt([[
 template<typename {}>
 {} {}({} {}) {{
     {}
 }}
 ]], {
-    i(1, "T"),
-    i(2, "void"),
-    i(3, "func"),
-    rep(1),
-    i(4, "value"),
-    i(0)
-})),
+        i(1, "T"),
+        i(2, "void"),
+        i(3, "func"),
+        rep(1),
+        i(4, "value"),
+        i(0)
+    })),
 
-s("concept", fmt([[
+    s("concept", fmt([[
 #include <concepts>
 
 template<typename T>
@@ -112,20 +112,20 @@ concept {} = requires(T a) {{
 }};
 ]], { i(1, "ConceptName"), i(0) })),
 
-s("requires", fmt([[
+    s("requires", fmt([[
 template<typename T>
 requires {}
 void {}(T {}) {{
     {}
 }}
 ]], {
-    i(1, "std::integral<T>"),
-    i(2, "func"),
-    i(3, "value"),
-    i(0)
-})),
+        i(1, "std::integral<T>"),
+        i(2, "func"),
+        i(3, "value"),
+        i(0)
+    })),
 
-s("ifc", fmt([[
+    s("ifc", fmt([[
 if constexpr ({}) {{
     {}
 }} else {{
@@ -133,20 +133,20 @@ if constexpr ({}) {{
 }}
 ]], { i(1, "cond"), i(2), i(0) })),
 
-s("fold", fmt([[
+    s("fold", fmt([[
 template<typename... Args>
 auto {}(Args... args) {{
     return ({} + ...);
 }}
 ]], { i(1, "sum"), i(2, "args") })),
 
-s("cexpr", fmt([[
+    s("cexpr", fmt([[
 constexpr auto {}(auto x) {{
     {}
 }}
 ]], { i(1, "func"), i(0) })),
 
-s("hashconstexpr", fmt([[
+    s("hashconstexpr", fmt([[
 constexpr uint64_t fnv1a(std::string_view str) {{
     uint64_t hash = 1469598103934665603ull;
     for (char c : str)
@@ -155,34 +155,34 @@ constexpr uint64_t fnv1a(std::string_view str) {{
 }}
 ]], {})),
 
--- =========================================================
--- C++23
--- =========================================================
+    -- =========================================================
+    -- C++23
+    -- =========================================================
 
-s("print23", fmt([[
+    s("print23", fmt([[
 #include <print>
 std::println("{}: {{}}", {});
 ]], { i(1, "label"), i(2, "value") })),
 
-s("expected", fmt([[
+    s("expected", fmt([[
 #include <expected>
 
 std::expected<{}, {}> {};
 ]], { i(1, "T"), i(2, "E"), i(3, "result") })),
 
-s("mdspan", fmt([[
+    s("mdspan", fmt([[
 #include <mdspan>
 
 std::mdspan<{}, std::extents<size_t, {}, {}>> {}({});
 ]], {
-    i(1, "float"),
-    i(2, "rows"),
-    i(3, "cols"),
-    i(4, "view"),
-    i(5, "data")
-})),
+        i(1, "float"),
+        i(2, "rows"),
+        i(3, "cols"),
+        i(4, "view"),
+        i(5, "data")
+    })),
 
-s("ranges", fmt([[
+    s("ranges", fmt([[
 #include <ranges>
 
 auto {} = {} | std::views::filter([](auto x) {{
@@ -190,11 +190,11 @@ auto {} = {} | std::views::filter([](auto x) {{
 }});
 ]], { i(1, "result"), i(2, "container"), i(0, "condition") })),
 
--- =========================================================
--- Systems / Performance
--- =========================================================
+    -- =========================================================
+    -- Systems / Performance
+    -- =========================================================
 
-s("timer", fmt([[
+    s("timer", fmt([[
 #include <chrono>
 
 auto start = std::chrono::high_resolution_clock::now();
@@ -206,7 +206,7 @@ std::chrono::duration<double> diff = end - start;
 std::cout << "Time: " << diff.count() << " s\n";
 ]], { i(0) })),
 
-s("simd", fmt([[
+    s("simd", fmt([[
 #include <immintrin.h>
 
 for (size_t {} = 0; {} < {}; {} += 8) {{
@@ -214,43 +214,42 @@ for (size_t {} = 0; {} < {}; {} += 8) {{
     {}
 }}
 ]], {
-    i(1,"i"),
-    i(1),
-    i(2,"N"),
-    i(1),
-    i(3,"vec"),
-    i(4,"data"),
-    i(1),
-    i(0)
-})),
+        i(1,"i"),
+        i(1),
+        i(2,"N"),
+        i(1),
+        i(3,"vec"),
+        i(4,"data"),
+        i(1),
+        i(0)
+    })),
 
-  
-s("avxadd", fmt([[
+    s("avxadd", fmt([[
 __m256 {} = _mm256_add_ps({}, {});
 ]], {
-    i(1,"result"),
-    i(2,"a"),
-    i(3,"b")
-})),
+        i(1,"result"),
+        i(2,"a"),
+        i(3,"b")
+    })),
 
-s("avxfma", fmt([[
+    s("avxfma", fmt([[
 __m256 {} = _mm256_fmadd_ps({}, {}, {});
 ]], {
-    i(1,"result"),
-    i(2,"a"),
-    i(3,"b"),
-    i(4,"c")
-})),
+        i(1,"result"),
+        i(2,"a"),
+        i(3,"b"),
+        i(4,"c")
+    })),
 
-s("avxstore", fmt([[
+    s("avxstore", fmt([[
 _mm256_storeu_ps(&{}[{}], {});
 ]], {
-    i(1,"data"),
-    i(2,"i"),
-    i(3,"vec")
-})),
+        i(1,"data"),
+        i(2,"i"),
+        i(3,"vec")
+    })),
 
-s("avxhsum", fmt([[
+    s("avxhsum", fmt([[
 float horizontal_sum(__m256 v) {{
     __m128 low = _mm256_castps256_ps128(v);
     __m128 high = _mm256_extractf128_ps(v, 1);
@@ -263,72 +262,72 @@ float horizontal_sum(__m256 v) {{
 }}
 ]], {}),
 
-s("prefetch", fmt([[
+    s("prefetch", fmt([[
 _mm_prefetch((const char*)(&{}[{}]), _MM_HINT_T0);
 ]], {
-    i(1,"data"),
-    i(2,"i")
-})),
+        i(1,"data"),
+        i(2,"i")
+    })),
 
-s("alignedalloc", fmt([[
+    s("alignedalloc", fmt([[
 {}* {} = static_cast<{}*>(std::aligned_alloc({}, {}));
 ]], {
-    i(1,"float"),
-    i(2,"ptr"),
-    i(1),
-    i(3,"32"),
-    i(4,"size")
-})),
+        i(1,"float"),
+        i(2,"ptr"),
+        i(1),
+        i(3,"32"),
+        i(4,"size")
+    })),
 
-s("avxmaskload", fmt([[
+    s("avxmaskload", fmt([[
 __m256 {} = _mm256_maskload_ps(&{}[{}], {});
 ]], {
-    i(1,"vec"),
-    i(2,"data"),
-    i(3,"i"),
-    i(4,"mask")
-})),
+        i(1,"vec"),
+        i(2,"data"),
+        i(3,"i"),
+        i(4,"mask")
+    })),
 
-s("avxmaskstore", fmt([[
+    s("avxmaskstore", fmt([[
 _mm256_maskstore_ps(&{}[{}], {}, {});
 ]], {
-    i(1,"data"),
-    i(2,"i"),
-    i(3,"mask"),
-    i(4,"vec")
-})),
+        i(1,"data"),
+        i(2,"i"),
+        i(3,"mask"),
+        i(4,"vec")
+    })),
 
-s("avx512loop", fmt([[
+    s("avx512loop", fmt([[
 for (size_t {} = 0; {} < {}; {} += 16) {{
     __m512 {} = _mm512_loadu_ps(&{}[{}]);
     {}
 }}
 ]], {
-    i(1,"i"),
-    i(1),
-    i(2,"N"),
-    i(1),
-    i(3,"vec"),
-    i(4,"data"),
-    i(1),
-    i(0)
-})),
+        i(1,"i"),
+        i(1),
+        i(2,"N"),
+        i(1),
+        i(3,"vec"),
+        i(4,"data"),
+        i(1),
+        i(0)
+    })),
 
-s("unroll", fmt([[
+    s("unroll", fmt([[
 #pragma unroll {}
 for (int {} = 0; {} < {}; ++{}) {{
     {}
 }}
 ]], {
-    i(1,"4"),
-    i(2,"i"),
-    i(2),
-    i(3,"N"),
-    i(2),
-    i(0)
-})),
+        i(1,"4"),
+        i(2,"i"),
+        i(2),
+        i(3,"N"),
+        i(2),
+        i(0)
+    })),
 
-s("stdsimd", fmt([[
+    s("stdsimd", fmt([[
 #include <experimental/simd>
 
 using simd_t = std::experimental::native_simd<{}>;
@@ -338,22 +337,22 @@ for (size_t {} = 0; {} < {}; {} += simd_t::size()) {{
     {}
 }}
 ]], {
-    i(1,"float"),
-    i(2,"i"),
-    i(2),
-    i(3,"N"),
-    i(2),
-    i(4,"vec"),
-    i(5,"data"),
-    i(2),
-    i(0)
-})),
+        i(1,"float"),
+        i(2,"i"),
+        i(2),
+        i(3,"N"),
+        i(2),
+        i(4,"vec"),
+        i(5,"data"),
+        i(2),
+        i(0)
+    })),
 
--- =========================================================
--- HIP (AMD GPU)
--- =========================================================
+    -- =========================================================
+    -- HIP (AMD GPU)
+    -- =========================================================
 
-s("hipkernel", fmt([[
+    s("hipkernel", fmt([[
 #include <hip/hip_runtime.h>
 
 __global__ void {}({}* {}) {{
@@ -361,145 +360,129 @@ __global__ void {}({}* {}) {{
     {}
 }}
 ]], {
-    i(1,"kernel"),
-    i(2,"float"),
-    i(3,"data"),
-    i(0)
-})),
+        i(1,"kernel"),
+        i(2,"float"),
+        i(3,"data"),
+        i(0)
+    })),
 
-s("hiplaunch", fmt([[
+    s("hiplaunch", fmt([[
 dim3 block({});
 dim3 grid(({} + block.x - 1) / block.x);
 
 hipLaunchKernelGGL({}, grid, block, 0, 0, {});
 hipDeviceSynchronize();
 ]], {
-    i(1,"256"),
-    i(2,"N"),
-    i(3,"kernel"),
-    i(4,"args")
-})),
+        i(1,"256"),
+        i(2,"N"),
+        i(3,"kernel"),
+        i(4,"args")
+    })),
 
-s("hipmalloc", fmt([[
+    s("hipmalloc", fmt([[
 {}* {};
 hipMalloc(&{}, {});
 ]], {
-    i(1,"float"),
-    i(2,"d_ptr"),
-    i(2),
-    i(3,"size")
-})),
+        i(1,"float"),
+        i(2,"d_ptr"),
+        i(2),
+        i(3,"size")
+    })),
 
-s("hipcpy", fmt([[
+    s("hipcpy", fmt([[
 hipMemcpy({}, {}, {}, {});
 ]], {
-    i(1,"dst"),
-    i(2,"src"),
-    i(3,"size"),
-    i(4,"hipMemcpyHostToDevice")
-})),
+        i(1,"dst"),
+        i(2,"src"),
+        i(3,"size"),
+        i(4,"hipMemcpyHostToDevice")
+    })),
 
-s("hipcheck", fmt([[
+    s("hipcheck", fmt([[
 #define HIP_CHECK(err) \
     if (err != hipSuccess) {{ \
         std::cerr << "HIP error: " << hipGetErrorString(err) << std::endl; \
         std::exit(EXIT_FAILURE); \
     }}
-]], {}),
+]], {})),
 
--- =========================================================
--- Standard Containers
--- =========================================================
+    -- =========================================================
+    -- Standard Containers
+    -- =========================================================
 
-s("vec", fmt([[
+    s("vec", fmt([[
 std::vector<{}> {};
 ]], { i(1,"T"), i(2,"vec") })),
 
-s("arr", fmt([[
+    s("arr", fmt([[
 std::array<{}, {}> {};
 ]], { i(1,"T"), i(2,"N"), i(3,"arr") })),
 
-s("map", fmt([[
+    s("map", fmt([[
 std::map<{}, {}> {};
 ]], { i(1,"Key"), i(2,"Value"), i(3,"map") })),
 
-s("umap", fmt([[
+    s("umap", fmt([[
 std::unordered_map<{}, {}> {};
 ]], { i(1,"Key"), i(2,"Value"), i(3,"umap") })),
 
-s("set", fmt([[
+    s("set", fmt([[
 std::set<{}> {};
 ]], { i(1,"T"), i(2,"set") })),
 
-s("uset", fmt([[
+    s("uset", fmt([[
 std::unordered_set<{}> {};
 ]], { i(1,"T"), i(2,"uset") })),
 
-s("deque", fmt([[
+    s("deque", fmt([[
 std::deque<{}> {};
 ]], { i(1,"T"), i(2,"dq") })),
 
-s("queue", fmt([[
+    s("queue", fmt([[
 std::queue<{}> {};
 ]], { i(1,"T"), i(2,"q") })),
 
-s("stack", fmt([[
+    s("stack", fmt([[
 std::stack<{}> {};
 ]], { i(1,"T"), i(2,"st") })),
 
-s("span", fmt([[
+    s("span", fmt([[
 std::span<{}> {};
 ]], { i(1,"T"), i(2,"sp") })),
 
--- =========================================================
--- Functions
--- =========================================================
+    -- =========================================================
+    -- Functions / Classes
+    -- =========================================================
 
-s("fn", fmt([[
+    s("fn", fmt([[
 {} {}({}) {{
     {}
 }}
 ]], {
-    i(1,"void"),
-    i(2,"function_name"),
-    i(3,""),
-    i(0)
-})),
+        i(1,"void"),
+        i(2,"function_name"),
+        i(3,""),
+        i(0)
+    })),
 
-s("lambda", fmt([[
+    s("lambda", fmt([[
 auto {} = [{}]({}) {{
     {}
 }};
 ]], {
-    i(1,"fn"),
-    i(2,"&"),
-    i(3,""),
-    i(0)
-})),
+        i(1,"fn"),
+        i(2,"&"),
+        i(3,""),
+        i(0)
+    })),
 
-s("inlinefn", fmt([[
-inline {} {}({}) {{
-    {}
-}}
-]], {
-    i(1,"void"),
-    i(2,"fn"),
-    i(3,""),
-    i(0)
-})),
-
--- =========================================================
--- Classes / Structs
--- =========================================================
-
-s("struct", fmt([[
+    s("struct", fmt([[
 struct {} {{
     {}
-
 }};
 ]], { i(1,"Name"), i(0) })),
 
-s("class", fmt([[
+    s("class", fmt([[
 class {} {{
 public:
     {}();
@@ -507,27 +490,15 @@ public:
 
 private:
     {}
-
 }};
 ]], {
-    i(1,"ClassName"),
-    rep(1),
-    rep(1),
-    i(0)
-})),
+        i(1,"ClassName"),
+        rep(1),
+        rep(1),
+        i(0)
+    })),
 
-s("ctor", fmt([[
-{}::{}({}) {{
-    {}
-}}
-]], {
-    i(1,"ClassName"),
-    rep(1),
-    i(2,""),
-    i(0)
-})),
-
-s("ruleof5", fmt([[
+    s("ruleof5", fmt([[
 {}();
 ~{}();
 
@@ -537,47 +508,25 @@ s("ruleof5", fmt([[
 {}({}&&) noexcept;
 {}& operator=({}&&) noexcept;
 ]], {
-    i(1,"Class"),
-    rep(1),
-    rep(1), rep(1),
-    rep(1), rep(1),
-    rep(1), rep(1),
-    rep(1), rep(1)
-})),
+        i(1,"Class"),
+        rep(1),
+        rep(1), rep(1),
+        rep(1), rep(1),
+        rep(1), rep(1),
+        rep(1), rep(1)
+    })),
 
--- =========================================================
--- Smart Pointers
--- =========================================================
+    -- =========================================================
+    -- Logic / Control Flow
+    -- =========================================================
 
-s("uptr", fmt([[
-std::unique_ptr<{}> {} = std::make_unique<{}>({});
-]], {
-    i(1,"T"),
-    i(2,"ptr"),
-    rep(1),
-    i(0)
-})),
-
-s("sptr", fmt([[
-std::shared_ptr<{}> {} = std::make_shared<{}>({});
-]], {
-    i(1,"T"),
-    i(2,"ptr"),
-    rep(1),
-    i(0)
-})),
-
--- =========================================================
--- Conditionals
--- =========================================================
-
-s("if", fmt([[
+    s("if", fmt([[
 if ({}) {{
     {}
 }}
 ]], { i(1,"condition"), i(0) })),
 
-s("ife", fmt([[
+    s("ife", fmt([[
 if ({}) {{
     {}
 }} else {{
@@ -585,59 +534,29 @@ if ({}) {{
 }}
 ]], { i(1,"condition"), i(2), i(0) })),
 
-s("switch", fmt([[
-switch ({}) {{
-case {}:
-    {}
-    break;
-default:
-    break;
-}}
-]], { i(1,"value"), i(2,"case_value"), i(0) })),
-
--- =========================================================
--- Loops
--- =========================================================
-
-s("fori", fmt([[
+    s("fori", fmt([[
 for (int {} = 0; {} < {}; ++{}) {{
     {}
 }}
 ]], {
-    i(1,"i"),
-    rep(1),
-    i(2,"N"),
-    rep(1),
-    i(0)
-})),
+        i(1,"i"),
+        rep(1),
+        i(2,"N"),
+        rep(1),
+        i(0)
+    })),
 
-s("forr", fmt([[
+    s("forr", fmt([[
 for (auto& {} : {}) {{
     {}
 }}
 ]], {
-    i(1,"elem"),
-    i(2,"container"),
-    i(0)
-})),
+        i(1,"elem"),
+        i(2,"container"),
+        i(0)
+    })),
 
-s("while", fmt([[
-while ({}) {{
-    {}
-}}
-]], { i(1,"condition"), i(0) })),
-
-s("dowhile", fmt([[
-do {{
-    {}
-}} while ({});
-]], { i(0), i(1,"condition") })),
-
--- =========================================================
--- Try / Catch
--- =========================================================
-
-s("try", fmt([[
+    s("try", fmt([[
 try {{
     {}
 }} catch (const std::exception& e) {{
@@ -645,21 +564,22 @@ try {{
 }}
 ]], { i(0) })),
 
--- =========================================================
--- Includes
--- =========================================================
+    -- =========================================================
+    -- Preprocessor
+    -- =========================================================
 
-s("once", fmt([[
+    s("once", fmt([[
 #pragma once
 
 {}
 ]], { i(0) })),
 
-s("inc", fmt([[
+    s("inc", fmt([[
 #include <{}>
 ]], { i(1,"iostream") })),
 
-s("incc", fmt([[
+    s("incc", fmt([[
 #include "{}"
 ]], { i(1,"header.hpp") })),
+
 })
